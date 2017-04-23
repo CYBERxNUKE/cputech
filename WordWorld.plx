596240caa91847a8074b19c74b7da4cd12d873f5
CYBERxNUKE <CYBERxNUKE@computertechs.org>
2017-04-23 12:14:22 -0500

    + ‘WordWorld.plx’ + ‘cookies.txt’ + ‘cxn - Copy.xml’ + ‘cxn.xml’ + ‘cxn.xml.bak’ + ‘guide.xml’ + ‘help.txt’ + ‘help.txt.bak’ + ‘homeshort.txt’ + ‘iptv-mx.zip’ + ‘iptv.xml’ + ‘new.xml’ + ‘new.xml.bak’ + ‘nuke.js’ + ‘nuke.m3u8’ + ‘nuke.m3u8-’ + ‘nuke.m3u8-down-old.bak’ + ‘nuke.m3u8-old-new’ + ‘nuke.m3u8-temp’ + ‘nuke.m3u8.bak’ + ‘nuke.m3u8.old’ + ‘nukeold.m3u8’ + ‘omfg.jpg’ + ‘repository.cxn.zip’ + ‘roku.xml’ + ‘servers.txt’ + ‘test.m3u8’ + ‘tp.xml’ + ‘ts.m3u8’ + ‘warning.png’

---
+++ WordWorld.plx
@@ -0,0 +1,77 @@
+version=1
+background=https://robdavidny.files.wordpress.com/2010/10/ww_poster_house.jpg
+title=WordWorld
+logo=https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Wordworldlogo11211.jpg/220px-Wordworldlogo11211.jpg
+description=A goofy group of animals are 'WordFriends' that teach preschool kids how to solve problems by building the right word. They journey on funny adventures together discovering that letters form words and that words have meanings. Characters like brainiac Frog and fun-loving Dog learn the alphabet on "WordWorld."/description
+
+# 2511981
+type=video
+name=S01/E01 Runaway O/Happy Birthday, Dog!
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=YbilYoexTWQ
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511982
+type=video
+name=S01/E02 There's an Ant in Every Giant/Chef Sheep
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=tQGAQ3UDgpU
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511984
+type=video
+name=S01/E03 Rocket to the Moon/The Birds
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=qRYiYmAiNZQ
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511985
+type=video
+name=S01/E04 The Mystery of the Disappearing Pie/Duck's Family Reunion
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=P0akaEi24bY
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511986
+type=video
+name=S01/E05 Sh-Sh-Shark!; Dog Wants to Play Ball
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=8c8Szr-YL-w
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511987
+type=video
+name=S01/E06 Pies, Pies, Pies!/Waterlogged
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=YL5Q70FWBos
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511988
+type=video
+name=S01/E07 Dog's Camping Adventure/V Is for Vacation
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=wnNroW9S4bI
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
+
+# 2511989
+type=html_youtube
+name=WordWorld
+date=2015-11-23
+URL=https://www.youtube.com/watch?v=j0pl8jyMAso&list=PLtBFFyhEIC7nS7Sx5038WSCk__p23utaj
+processor=http://www.navixtreme.com/proc/youtube
+player=default
+rating=-1.00
---
+++ nuke.js
@@ -0,0 +1,115 @@
+window.channels = [
+  {
+    "title": "TSN 1",
+	"mediaid": "Sports HD",
+    "image": "http://www.streamtvbox.com/site/placeholder.png",
+    "sources": [
+      {
+        "file": "plugin://program.plexus/?url=13139e30694a4902cc4bc8e0859d64c9851c80cb&mode=1&name=CYBERxNUKE+Channel&mode=12",
+        "default": "true",
+        "type": "rtmp"
+      }
+  },{
+    "title": "AMC - Acestream",
+	"mediaid": "Sports HD",
+    "image": "http://nowwatchtvlive.me/wp-content/uploads/2015/09/amc_acestream.png",
+    "sources": [
+      {
+        "file": "plugin://program.plexus/?url=9d490c73b8376b3410d67bbe302947f2d3ab9baa&mode=1&name=CYBERxNUKE+Channel&mode=12",
+        "default": "true",
+        "type": "rtmp"
+      }
+  },
+
+];
+window.channels.sort( function(a,b) 
+{
+	if(a.mediaid < b.mediaid)
+		return -1;
+	if(a.mediaid > b.mediaid)
+		return 1;
+	if(a.title < b.title)
+		return -1;
+	if(a.title > b.title)
+		return 1;
+	return 0;
+});
+
+
+
+//---------- Do NOT edit anything below this line!! ----------//
+
+jQuery(document).ready(function() {
+  var $, channelSelectContainer, jwParent, aspectRatio, i, iChannel, jwp, jwpResize;
+  $ = jQuery;
+  channelSelectContainer = $('#channel-select');
+  channelSelectContainer.before('<div id="category-select" style="text-align:center;line-height:50px;"></div>');
+  categorySelectContainer = $('#category-select');
+  i = 0;
+  a = 0;
+  category = " ";
+  while (i < channels.length) {
+    iChannel = channels[i];
+    if(category != iChannel.mediaid) {
+    	category = iChannel.mediaid;
+    	a += 1;
+    	if(a === 7) categorySelectContainer.append('<br>');
+    	categorySelectContainer.append('<a href="#" class="category-selector category-button" data-category="' + iChannel.mediaid + '">' + iChannel.mediaid + '</a>');
+    }
+    i += 1;
+  }
+  categorySelectContainer.append('<a href="#" class="category-selector category-button" data-category="View all">View all</a>');
+
+  $('.category-selector').click(function(e) {
+  	e.preventDefault();
+  	var caller = $(this);
+  	channelSelectContainer.slideUp(500, function() {
+  		channelSelectContainer.html('');
+	  	it = 0;
+	  	while (it < channels.length) {
+	    	iChannel = channels[it];
+	    	if(iChannel.mediaid == caller.data('category') || caller.data('category') == "View all") {
+	    		channelSelectContainer.append("      <a id=\"channel-" + it + "\"      class=\"channel-select-link" + (it === 0 ? ' selected' : '') + "\"      href=\"#\">        " + (iChannel.title != null ? iChannel.title : 'Channel ' + it) + "      </a>      ");
+	    	}
+	    	it += 1;
+	  	}
+	  	channelSelectContainer.slideDown(500);
+	  	$('.channel-select-link').click(function(e) {
+		    var currentItem, jw, newItem, selectedChannel;
+		    e.preventDefault();
+		    $('.channel-select-link').removeClass('selected');
+		    $(this).addClass('selected');
+		    selectedChannel = parseInt($(this).attr("id").replace("channel-", ""));
+		    jw = jwplayer("video-player");
+		    currentItem = jw.getPlaylistItem();
+		    newItem = jw.getPlaylistItem(selectedChannel);
+		    console.log({
+		      currentItem: currentItem,
+		      newItem: newItem,
+		      selectedChannel: selectedChannel,
+		      jw: jw
+		    });
+		    if (jw.getPlaylistItem(selectedChannel) !== jw.getPlaylistItem()) {
+		      return jw.playlistItem(selectedChannel)
+			  document.write(selectedChannel)
+			  document.write(urlkey1);
+		    }
+		 });
+	});
+  });
+  /*
+      Correctly resize the player, as the setup options are not working
+  */
+
+  aspectRatio = 16 / 9;
+  jwp = jwplayer("video-player");
+  jwParent = $("#video-player-container");
+  jwpResize = function() {
+    var jwplayerWidth;
+    jwp = jwplayer("video-player");
+    jwplayerWidth = jwParent.width();
+    jwp.resize(jwplayerWidth, jwplayerWidth / aspectRatio);
+    return $('#chat-container').height(jwplayerWidth / aspectRatio);
+  };
+  return null;
+});
